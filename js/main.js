0ea95dff6edae0cbba22b06b43d4d55c595f182c
Hamidreza Toloueinasab <Hamidrezatolouienasab@gmail.com>
2022-09-11 16:32:49 +0430

    End of Services

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ const navbar = document.querySelector(".nav");
 toggler.addEventListener("click", (e) => {
     navbar.classList.toggle("nav__expanded");
 });
+ 
+const tabs = document.querySelectorAll(".tab");
+const tabContents = document.querySelectorAll(".tab-content");
+tabs.forEach((tab) => {
+    tab.addEventListener("click", (e) => {
+        const targeTabContent = document.querySelector(`#${tab.dataset.tabTarget}`);
+        // remove previous active classes
+        tabs.forEach((tab) => tab.classList.remove("active"));
+        tabContents.forEach((tabContent) =>
+            tabContent.classList.remove("active"));
+        // add new active classes
+        tab.classList.add("active");
+        targeTabContent.classList.add("active");
+    });
+});
